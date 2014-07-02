b469e1e9f33c6f60feecbbeb47e30260485dd68b
Thomas Parslow <tom@almostobsolete.net>
2014-07-02 16:44:26 +0100

    Testing

--- aib_python/recipes/deploy.rb
+++ aib_python/recipes/deploy.rb
@@ -1,11 +1,19 @@
 include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
-  if deploy[:application_type] != 'python'
-    Chef::Log.debug("Skipping python deploy for application #{application} as it is not a python app")
+  
+  Chef::Log.info("Stuff: #{deploy[:application_type]}")
+  if deploy[:application_type] != 'custom'
+    Chef::Log.info("Skipping python deploy for application #{application} as it is not a python app")
     next
   end
-
+  
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+  
   opsworks_deploy do
     deploy_data deploy
     app application
